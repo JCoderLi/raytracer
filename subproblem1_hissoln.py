67f77ec05c178b304ff2e64ad21defae3d20b960
JCoderLi <githubjli@gmail.com>
2020-04-02 21:53:47 -0400

    deleetete

--- subproblem1_hissoln.py
+++
@@ -1,6 +0,0 @@
-# operations:
-# find length (magnitude), sqrt X**2, Y**2, Z**2
-# normalize operator: direction
-#    divide vector by magnitude
-# dot product: [a,b,c] * [d,e,f] = sqrt(ad+be+cf) ?
-# v + v, v - v, v*2, v/

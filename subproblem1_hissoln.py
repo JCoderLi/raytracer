f75950f4df24334c2a6fd6f6de694ed771128c58
JCoderLi <githubjli@gmail.com>
2020-04-04 18:22:53 -0400

    fax

---
+++ subproblem1_hissoln.py
@@ -0,0 +1,13 @@
+# operations:
+# find length (magnitude), sqrt X**2, Y**2, Z**2
+# normalize operator: direction
+#    divide vector by magnitude
+# dot product: [a,b,c] * [d,e,f] = sqrt(ad+be+cf) ?
+# v + v, v - v, v*2, v/2
+
+# magnitude/dotproduct
+# magnitude is just sqrt dotproduct of itself
+
+
+
+
---
+++ subproblem2.py
@@ -0,0 +1,9 @@
+# create a 3 x 2 pixel image
+# FIRST ROW:
+# RED = (1,0,0)
+# GREEN = (0,1,0)
+# BLUE = (0,0,1)
+# SECOND ROW
+# R+G = (1,1,0)
+# R+G+B = (1,1,1)
+# R * .001 = (.001,0,0)
